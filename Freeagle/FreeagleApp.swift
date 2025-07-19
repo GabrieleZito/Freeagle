b7e550dea4d7450770eda0e754a928b718a69d62
gabriele <gabriele1413@gmail.com>
2025-07-19 08:52:16 +0200

    Initial Commit

---
+++ Freeagle/FreeagleApp.swift
@@ -0,0 +1,17 @@
+//
+//  FreeagleApp.swift
+//  Freeagle
+//
+//  Created by Gabriele Zito on 19/07/25.
+//
+
+import SwiftUI
+
+@main
+struct FreeagleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
